5724e1d0bc039cec7a39fa0e702654c6e332f38f
anabcorreia <anabeatrizcorreiaribeiro010@gmail.com>
2023-09-29 08:32:58 -0300

    index

--- inscricoes.php
+++ inscricoes.php
@@ -26,7 +26,6 @@
                         <li><a href="suporte.php">SUPORTE</a></li>
                     </ul>
                     </div>
-                    </section>
 
                     
         <aside>
@@ -49,14 +48,9 @@
                 <script src="calendario/script.js"></script>
 
         
-        </aside>
-        <footer>
-    <br>
-    <br>
-    </footer>
-    </div>
-
-    <div>
+        </aside> 
+        <section>
+         <div>
         <h1>Login</h1>
         <form action="testLogin.php" method="POST">
             <div>
@@ -66,6 +60,8 @@
             <input type="password" name="senha" placeholder="Senha">
             <br><br>
             <a href="index.html" class="submit">Login</a>
+
+            
         
     </div>
     <h1>Adm</h1>
@@ -73,6 +69,15 @@
         <a href="./cadastro-e-login/login_adm.php">Login Adm</a>
         <a href="./cadastro-e-login/cadastro.php">Cadastro Adm</a>
     </div>
+
+</section>
+        <footer>
+    <br>
+    <br>
+    </footer>
+    </div>
+
+  
 
 </body>
 </html>
