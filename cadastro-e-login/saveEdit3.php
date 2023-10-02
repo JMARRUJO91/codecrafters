730ab64c677f53fc36a310532f2e297c9034240e
FAB10-H3NRIQUE <fabiohpereira2007@hotmail.com>
2023-10-02 13:31:32 -0300

    asdf

--- cadastro-e-login/saveEdit3.php
+++ cadastro-e-login/saveEdit3.php
@@ -1,17 +1,38 @@
 <?php
-    // isset -> serve para saber se uma variável está definida
-    include_once('config.php');
-    if(isset($_POST['update']))
-    {
-        $nome = $_POST['nome'];
-        $senha = $_POST['senha'];
-        
-        $sqlInsert = "UPDATE adm 
-        SET nome='$nome',senha='$senha'
-        WHERE id=$id";
-        $result = $conexao->query($sqlInsert);
-        print_r($result);
-    }
-    header('Location: adm_lista.php');
+include_once('config.php');
 
+if (isset($_POST['update'])) {
+    $id_individual = $_POST['id_individual'];
+    $nome = $_POST['nome'];
+    $idade = $_POST['idade'];
+    $modalidade = $_POST['modalidade'];
+    $email = $_POST['email'];
+    $cidade = $_POST['cidade'];
+    $serie = $_POST['serie'];
+    $telefone = $_POST['telefone'];
+
+    // Usar declarações preparadas para evitar injeção SQL
+    $sqlUpdate = "UPDATE individual 
+                  SET nome=?, idade=?, modalidade=?, email=?, cidade=?, serie=?, telefone=?
+                  WHERE id_individual=?";
+    $stmt = $conexao->prepare($sqlUpdate);
+    
+    // Verificar se a preparação da consulta foi bem-sucedida
+    if ($stmt) {
+        $stmt->bind_param("sssssssi", $nome, $idade, $modalidade, $email, $cidade, $serie, $telefone, $id_individual);
+    
+        if ($stmt->execute()) {
+            echo "Registro atualizado com sucesso.";
+        } else {
+            echo "Erro ao atualizar o registro: " . $stmt->error;
+        }
+        
+        // Fechar a declaração preparada após o uso
+        $stmt->close();
+    } else {
+        echo "Erro na preparação da consulta: " . $conexao->error;
+    }
+}
+
+header('Location: indi_lista.php');
 ?>
